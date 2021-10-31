fad09977e3e0f5e112dbbf4899c5d97d885e455a
Gyandeep <gyandeepmehra370@gmail.com>
2021-10-31 11:25:22 +0530

    Add Print Z

---
+++ Basics Of Programming/Getting Started/Print_Z.java
@@ -0,0 +1,10 @@
+public class Print_Z {
+  public static void main(String[] args) {
+    //write your code here
+    System.out.println("*****");
+    System.out.println("   * ");
+    System.out.println("  *  ");
+    System.out.println(" *   ");
+    System.out.println("*****");
+  }
+}
