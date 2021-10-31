b351308156b48f4dcc3aa271246399163e93d780
Gyandeep <gyandeepmehra370@gmail.com>
2021-10-31 14:09:37 +0530

    Add pattern 2

---
+++ Basics Of Programming/Patterns/Pattern_2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Pattern_2 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        // write ur code here
+        int n = scn.nextInt();
+        for(int i=1;i<=n;++i){
+            for(int j=1;j<=(n-i+1);++j){
+                System.out.print("*" + "\t");
+            }
+            System.out.println();//it press enter
+        }
+        scn.close();
+    }
+}
