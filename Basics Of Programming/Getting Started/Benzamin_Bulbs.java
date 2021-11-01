7a9f6feeee65ed7b5e99b731d4c37404812e0326
Gyandeep <gyandeepmehra370@gmail.com>
2021-11-01 09:42:45 +0530

    Add curious case of Benzamin bulbs

---
+++ Basics Of Programming/Getting Started/Benzamin_Bulbs.java
@@ -0,0 +1,20 @@
+//Curious case of benzamin bulbs
+import java.util.*;
+  
+  public class Benzamin_Bulbs{
+  public static void main(String[] args) {
+    // write your code here  
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int i=1;
+    while (i <= n) {
+        if (i * i <= n) {
+            System.out.println(i * i);
+        } else {
+            break;
+        }
+        ++i;
+    }
+    scn.close();
+   }
+  }
