c381cd1e37e8e1367f57ef4c86e329f0b26e5c6e
Gyandeep <gyandeepmehra370@gmail.com>
2021-11-08 08:58:59 +0530

    Add pattern 20

---
+++ Basics Of Programming/Patterns/Pattern_20.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Pattern_20 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        // write ur code here
+        int n = scn.nextInt();
+        for(int i=1;i<=n;++i){
+            for(int j=1;j<=n;++j){
+                if(j==1 || j==n){
+                    System.out.print("*" + "\t");
+                }else if(i>=(n/2)+1 && (i==j || (i+j)%(n+1) ==0)){
+                    System.out.print("*" + "\t");
+                }else{
+                    System.out.print("\t");
+                }
+            }
+            System.out.println();
+        }
+        scn.close();
+    }
+}
