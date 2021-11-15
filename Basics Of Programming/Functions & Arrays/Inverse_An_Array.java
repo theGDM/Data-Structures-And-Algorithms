f1c193f76869fa3da48ab6d7ec1b29567fd87d48
Gyandeep <gyandeepmehra370@gmail.com>
2021-11-15 09:17:24 +0530

    Add inverse an array

---
+++ Basics Of Programming/Functions & Arrays/Inverse_An_Array.java
@@ -0,0 +1,37 @@
+import java.io.*;
+
+public class Inverse_An_Array{
+  public static void display(int[] a){
+    StringBuilder sb = new StringBuilder();
+
+    for(int val: a){
+      sb.append(val + "\n");
+    }
+    System.out.println(sb);
+  }
+
+  public static int[] inverse(int[] a){
+    // write your code here
+    int[] b = new int[a.length];
+    int j = 0;
+    for(int i = 0;i<a.length;++i){
+        j = a[i];
+        b[j] = i;
+    }
+    return b;
+  }
+
+public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(br.readLine());
+    int[] a = new int[n];
+    for(int i = 0; i < n; i++){
+       a[i] = Integer.parseInt(br.readLine());
+    }
+
+    int[] inv = inverse(a);
+    display(inv);
+ }
+
+}
