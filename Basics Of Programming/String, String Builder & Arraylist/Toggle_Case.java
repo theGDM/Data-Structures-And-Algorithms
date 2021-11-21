852552ef6f385ab9ffd4aed5efa64a7f9841f599
Gyandeep <gyandeepmehra370@gmail.com>
2021-11-21 23:36:26 +0530

    Add togle case

---
+++ Basics Of Programming/String, String Builder & Arraylist/Toggle_Case.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Toggle_Case {
+
+	public static String toggleCase(String str){
+		//write your code here
+		//can not do inplace
+		String res = "";
+        for(int i = 0;i < str.length();++i){
+            char ch = str.charAt(i);
+            
+            if( ch >= 'a' && ch <= 'z'){
+                res = res + (char)(ch - 32);
+            }else{
+                res = res + (char)(ch + 32);
+            }
+        }
+		return res;
+	}
+	
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		String str = scn.next();
+        System.out.println(toggleCase(str));
+        scn.close();
+	}
+
+}
