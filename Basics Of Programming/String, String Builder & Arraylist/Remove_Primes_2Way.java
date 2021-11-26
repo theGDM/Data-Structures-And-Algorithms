ef7b85d3e8c262653d52083966cececc55e50268
Gyandeep <gyandeepmehra370@gmail.com>
2021-11-26 11:52:34 +0530

    Add remove primes 2nd way

---
+++ Basics Of Programming/String, String Builder & Arraylist/Remove_Primes_2Way.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Remove_Primes_2Way {
+
+    public static Boolean isPrime(int n) {
+        for (int i = 2; i * i <= n; ++i) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+  public static void solution(ArrayList<Integer> al) {
+    // write your code here
+    for (int i = al.size()-1; i >= 0; --i) {
+        if (isPrime(al.get(i)) == true) {
+            al.remove(i);
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    ArrayList<Integer> al = new ArrayList<>();
+    for (int i = 0 ; i < n; i++) {
+      al.add(scn.nextInt());
+    }
+    solution(al);
+    System.out.println(al);
+    scn.close();
+  }
+
+}
