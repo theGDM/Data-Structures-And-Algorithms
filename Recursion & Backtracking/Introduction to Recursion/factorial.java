9905c40bd5e5e01d1cb3becd3ffc2e93ad34564b
Gyandeep <gyandeepmehra370@gmail.com>
2021-12-06 09:19:51 +0530

    factorial using recursion

---
+++ Recursion & Backtracking/Introduction to Recursion/factorial.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class factorial {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int res = factn(n);
+        System.out.println(res);
+        scn.close();
+    }
+
+    public static int factn(int n){
+        if(n == 0) return 1;
+        //Faith
+        int factnm1 = factn(n - 1);
+        //Meeting expectation with faith
+        return factnm1 * n;
+    }
+
+}
