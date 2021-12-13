5f5353f4dcf0abc076ebef8b7838f272151dc464
Gyandeep <gyandeepmehra370@gmail.com>
2021-12-13 10:05:41 +0530

    Add get maze path with jumps

---
+++ Recursion & Backtracking/Recursion with Arraylist/get_maze_path_with_jumps.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+public class get_maze_path_with_jumps {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int rows = scn.nextInt();
+        int cols = scn.nextInt();
+        ArrayList<String> res = getMazePaths(0, 0, rows - 1, cols - 1);
+        System.out.println(res);
+        scn.close();
+    }
+
+    // sr - source row
+    // sc - source column
+    // dr - destination row
+    // dc - destination column
+
+    public static ArrayList<String> getMazePaths(int sr, int sc, int dr, int dc) {
+        // positive base case
+        if (sr == dr && sc == dc) {
+            ArrayList<String> base = new ArrayList<>();
+            base.add("");
+            return base;
+        }
+
+        // negative base case
+        if (sr > dr || sc > dc) {
+            ArrayList<String> base = new ArrayList<>();
+            return base;
+        }
+
+        ArrayList<String> ans = new ArrayList<>();
+
+        // moving right and getting all the paths with jumps(faith call)
+        for (int jump = 1; jump <= dc; ++jump) {
+            ArrayList<String> rightPaths = getMazePaths(sr, sc + jump, dr, dc);
+            for (String str : rightPaths) {
+                ans.add("h" + jump + str);
+            }
+        }
+
+        // moving down and getting all the paths with jumps(faith call)
+        for (int jump = 1; jump <= dr; ++jump) {
+            ArrayList<String> downPaths = getMazePaths(sr + jump, sc, dr, dc);
+            for (String str : downPaths) {
+                ans.add("v" + jump + str);
+            }
+        }
+
+        // moving diagonally and getting all the paths with jumps(faith call)
+        for (int jump = 1; jump <= Math.min(dr, dc); ++jump) {
+            ArrayList<String> diagonalPaths = getMazePaths(sr + jump, sc + jump, dr, dc);
+            for (String str : diagonalPaths) {
+                ans.add("d" + jump + str);
+            }
+        }
+
+        return ans;
+    }
+
+}
