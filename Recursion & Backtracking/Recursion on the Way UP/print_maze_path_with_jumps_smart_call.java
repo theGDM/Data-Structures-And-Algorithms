8db060dfb1ed03d92367cb7699265db4b862ea48
Gyandeep <gyandeepmehra370@gmail.com>
2021-12-15 09:23:53 +0530

    Add print maze path with jumps using smart call

---
+++ Recursion & Backtracking/Recursion on the Way UP/print_maze_path_with_jumps_smart_call.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class print_maze_path_with_jumps_smart_call {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int rows = scn.nextInt();
+        int cols = scn.nextInt();
+        printMazePaths(0, 0, rows - 1, cols - 1, "");
+        scn.close();
+    }
+
+    // sr - source row
+    // sc - source column
+    // dr - destination row
+    // dc - destination column
+    public static void printMazePaths(int sr, int sc, int dr, int dc, String output) {
+        // positive base case
+        if (sr == dr && sc == dc) {
+            System.out.println(output);
+            return;
+        }
+
+        // moving right and getting all the paths with jumps(faith call)
+        for (int jump = 1; sc + jump <= dc; ++jump) {
+            printMazePaths(sr, sc + jump, dr, dc, output + "h" + jump);
+        }
+
+        // moving down and getting all the paths with jumps(faith call)
+        for (int jump = 1; sr + jump <= dr; ++jump) {
+            printMazePaths(sr + jump, sc, dr, dc, output + "v" + jump);
+        }
+
+        // moving diagonally and getting all the paths with jumps(faith call)
+        for (int jump = 1; sc + jump <= dc && sr + jump <= dr; ++jump) {
+            printMazePaths(sr + jump, sc + jump, dr, dc, output + "d" + jump);
+        }
+    }
+
+}
