47b52e8e2cb5c2c6ecc0f13ac1a279daec1bdcce
Gyandeep <gyandeepmehra370@gmail.com>
2021-12-18 09:24:59 +0530

    Modification

--- Basics Of Programming/Patterns/Radial_Rectangular.java
+++ Basics Of Programming/Patterns/Radial_Rectangular.java
@@ -4,20 +4,20 @@ public class Radial_Rectangular {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
-        //upper half
-        int row = 2*n-1;
+        // upper half
+        int row = 2 * n - 1;
         for (int i = 1; i <= row / 2 + 1; ++i) {
             int m = n;
-            //Decreasing part
+            // Decreasing part
             for (int j = 1; j <= (i - 1); ++j) {
                 System.out.print(m + "  ");
                 --m;
             }
-            //Constant part
+            // Constant part
             for (int k = 1; k <= row - 2 * (i - 1); ++k) {
-                System.out.print(m + "  "); 
+                System.out.print(m + "  ");
             }
-            //Decresing part
+            // Decresing part
             m = m + 1;
             for (int l = 1; l <= (i - 1); ++l) {
                 System.out.print(m + "  ");
@@ -25,21 +25,21 @@ public class Radial_Rectangular {
             }
             System.out.println();
         }
-        //upper half
+        // upper half
         for (int i = 1; i <= row / 2; ++i) {
             int m = n;
-            //Decreasing part
-            for (int j = 1; j <= (row/2)-i; ++j) {
+            // Decreasing part
+            for (int j = 1; j <= (row / 2) - i; ++j) {
                 System.out.print(m + "  ");
                 --m;
             }
-            //Constant part
+            // Constant part
             for (int k = 1; k <= 2 * (i + 1) - 1; ++k) {
                 System.out.print(m + "  ");
             }
-            //Decresing part
+            // Decresing part
             m = m + 1;
-            for (int l = 1; l <= (row/2)-i; ++l) {
+            for (int l = 1; l <= (row / 2) - i; ++l) {
                 System.out.print(m + "  ");
                 ++m;
             }
