07a89e6f7559f1234053a06a566acab614d76a93
theGDM <gyandeepmehra370@gmail.com>
2022-03-06 19:31:49 +0530

    Create Node With Maximum Subtree Sum.java

---
+++ Basic Data Structures/Generic Tree/Node With Maximum Subtree Sum.java
@@ -0,0 +1,86 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  private static class Node {
+    int data;
+    ArrayList<Node> children = new ArrayList<>();
+  }
+
+  public static void display(Node node) {
+    String str = node.data + " -> ";
+    for (Node child : node.children) {
+      str += child.data + ", ";
+    }
+    str += ".";
+    System.out.println(str);
+
+    for (Node child : node.children) {
+      display(child);
+    }
+  }
+
+  public static Node construct(int[] arr) {
+    Node root = null;
+
+    Stack<Node> st = new Stack<>();
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] == -1) {
+        st.pop();
+      } else {
+        Node t = new Node();
+        t.data = arr[i];
+
+        if (st.size() > 0) {
+          st.peek().children.add(t);
+        } else {
+          root = t;
+        }
+
+        st.push(t);
+      }
+    }
+
+    return root;
+  }
+
+  public static int maxS;
+  public static int maxNodeS;
+  public static int sum(Node node){
+    if(node == null){ //no tree
+      return 0;
+    }
+
+    int cSum = 0;
+    for(Node child : node.children){
+      cSum += sum(child);//getting sum of all subtrees rooted at current node
+    }
+
+    cSum += node.data;
+    if(cSum > maxS){
+      maxS = cSum;
+      maxNodeS = node.data;
+    }
+
+    return cSum;
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    int[] arr = new int[n];
+    String[] values = br.readLine().split(" ");
+    for (int i = 0; i < n; i++) {
+      arr[i] = Integer.parseInt(values[i]);
+    }
+
+    Node root = construct(arr);
+
+    maxS = Integer.MIN_VALUE;
+    maxNodeS = Integer.MIN_VALUE;
+    sum(root);
+
+    System.out.println(maxNodeS + "@" + maxS);
+  } 
+
+}
