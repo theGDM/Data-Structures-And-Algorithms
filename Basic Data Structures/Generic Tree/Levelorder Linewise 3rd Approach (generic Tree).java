67c898bdf2a9726d0f13249555ef29906f0624b2
theGDM <gyandeepmehra370@gmail.com>
2022-03-09 17:46:54 +0530

    Create Levelorder Linewise 3rd Approach (generic Tree).java

---
+++ Basic Data Structures/Generic Tree/Levelorder Linewise 3rd Approach (generic Tree).java
@@ -0,0 +1,141 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  private static class Node {
+    int data;
+    ArrayList<Node> children;
+
+    Node(){
+      this.data = 0;
+      this.children = new ArrayList<>();
+    }
+
+    Node(int data){
+      this.data = data;
+      this.children = new ArrayList<>();
+    }
+  }
+
+  public static void display(Node node) {
+    String str = node.data + " -> ";
+    for (Node child : node.children) {
+      str += child.data + ", ";
+    }
+    str += ".";
+    System.out.println(str);
+
+    for (Node child : node.children) {
+      display(child);
+    }
+  }
+
+  public static Node construct(int[] arr) {
+    Node root = null;
+
+    Stack<Node> st = new Stack<>();
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] == -1) {
+        st.pop();
+      } else {
+        Node t = new Node();
+        t.data = arr[i];
+
+        if (st.size() > 0) {
+          st.peek().children.add(t);
+        } else {
+          root = t;
+        }
+
+        st.push(t);
+      }
+    }
+
+    return root;
+  }
+
+  public static int size(Node node) {
+    int s = 0;
+
+    for (Node child : node.children) {
+      s += size(child);
+    }
+    s += 1;
+
+    return s;
+  }
+
+  public static int max(Node node) {
+    int m = Integer.MIN_VALUE;
+
+    for (Node child : node.children) {
+      int cm = max(child);
+      m = Math.max(m, cm);
+    }
+    m = Math.max(m, node.data);
+
+    return m;
+  }
+
+  public static int height(Node node) {
+    int h = -1;
+
+    for (Node child : node.children) {
+      int ch = height(child);
+      h = Math.max(h, ch);
+    }
+    h += 1;
+
+    return h;
+  }
+
+  public static void traversals(Node node){
+    System.out.println("Node Pre " + node.data);
+
+    for(Node child: node.children){
+      System.out.println("Edge Pre " + node.data + "--" + child.data);
+      traversals(child);
+      System.out.println("Edge Post " + node.data + "--" + child.data);
+    }
+
+    System.out.println("Node Post " + node.data);
+  }
+
+  public static void levelOrderLinewise(Node node){
+    if(node == null){//no tree present
+      return;
+    }
+
+    Queue<Node> q = new ArrayDeque<>();
+    q.add(node);
+    
+    while(q.size() != 0){
+      int size = q.size();
+      while(size-- > 0){
+        Node rnode = q.remove(); //remove
+        System.out.print(rnode.data + " "); //print
+        for(Node child : rnode.children){ //add
+          q.add(child);
+        }
+      }
+      //if size becomes 0, that means one level is completed and we need to print
+      //a new line
+      System.out.println();
+    }
+    
+  }  
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    int[] arr = new int[n];
+    String[] values = br.readLine().split(" ");
+    for (int i = 0; i < n; i++) {
+      arr[i] = Integer.parseInt(values[i]);
+    }
+
+    Node root = construct(arr);
+    levelOrderLinewise(root);
+  }
+
+}
