60ffce1e053fe8098259a6e1449bb5394d220d85
theGDM <gyandeepmehra370@gmail.com>
2022-03-26 09:56:45 +0530

    Create Is A Binary Search Tree(Inorder Approach).java

---
+++ Basic Data Structures/Binary Tree/Is A Binary Search Tree(Inorder Approach).java
@@ -0,0 +1,159 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  public static class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data, Node left, Node right) {
+      this.data = data;
+      this.left = left;
+      this.right = right;
+    }
+  }
+
+  public static class Pair {
+    Node node;
+    int state;
+
+    Pair(Node node, int state) {
+      this.node = node;
+      this.state = state;
+    }
+  }
+
+  public static Node construct(Integer[] arr) {
+    Node root = new Node(arr[0], null, null);
+    Pair rtp = new Pair(root, 1);
+
+    Stack<Pair> st = new Stack<>();
+    st.push(rtp);
+
+    int idx = 0;
+    while (st.size() > 0) {
+      Pair top = st.peek();
+      if (top.state == 1) {
+        idx++;
+        if (arr[idx] != null) {
+          top.node.left = new Node(arr[idx], null, null);
+          Pair lp = new Pair(top.node.left, 1);
+          st.push(lp);
+        } else {
+          top.node.left = null;
+        }
+
+        top.state++;
+      } else if (top.state == 2) {
+        idx++;
+        if (arr[idx] != null) {
+          top.node.right = new Node(arr[idx], null, null);
+          Pair rp = new Pair(top.node.right, 1);
+          st.push(rp);
+        } else {
+          top.node.right = null;
+        }
+
+        top.state++;
+      } else {
+        st.pop();
+      }
+    }
+
+    return root;
+  }
+
+  public static void display(Node node) {
+    if (node == null) {
+      return;
+    }
+
+    String str = "";
+    str += node.left == null ? "." : node.left.data + "";
+    str += " <- " + node.data + " -> ";
+    str += node.right == null ? "." : node.right.data + "";
+    System.out.println(str);
+
+    display(node.left);
+    display(node.right);
+  }
+
+  public static int height(Node node) {
+    if (node == null) {
+      return -1;
+    }
+
+    int lh = height(node.left);
+    int rh = height(node.right);
+
+    int th = Math.max(lh, rh) + 1;
+    return th;
+  }
+
+  public static int max(Node node){
+    if(node == null){
+      return Integer.MIN_VALUE;
+    }
+
+    int lmax = max(node.left);
+    int rmax = max(node.right);
+
+    return Math.max(rmax, Math.max(lmax, node.data));
+  }
+
+  public static int min(Node node){
+    if(node == null){
+      return Integer.MAX_VALUE;
+    }
+
+    int lmin = min(node.left);
+    int rmin = min(node.right);
+
+    return Math.min(rmin, Math.min(lmin, node.data));
+  }
+
+  public static void getInorder(Node node, ArrayList<Integer> in){
+    if(node == null){
+      return;
+    }
+
+    getInorder(node.left, in);//pre area
+    in.add(node.data);//in area
+    getInorder(node.right, in);//post area
+  }
+
+  public static boolean isBST(Node node){
+   ArrayList<Integer> inorder = new ArrayList<>(); 
+   getInorder(node, inorder);
+   
+   for(int i = 0;i < inorder.size() - 1; ++i){
+     if(inorder.get(i) >= inorder.get(i + 1)){
+       return false;
+     }
+   }
+
+   return true;
+  }
+  
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    Integer[] arr = new Integer[n];
+    String[] values = br.readLine().split(" ");
+    for (int i = 0; i < n; i++) {
+      if (values[i].equals("n") == false) {
+        arr[i] = Integer.parseInt(values[i]);
+      } else {
+        arr[i] = null;
+      }
+    }
+
+    Node root = construct(arr);
+    
+    // write your code here
+    boolean ans = isBST(root);
+    System.out.println(ans);
+  }
+
+}
