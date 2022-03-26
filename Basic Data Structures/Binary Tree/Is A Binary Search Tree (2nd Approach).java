32d5db1bbd1f6824f4856f69fd55294a727366ee
theGDM <gyandeepmehra370@gmail.com>
2022-03-26 10:16:17 +0530

    Create Is A Binary Search Tree (2nd Approach).java

---
+++ Basic Data Structures/Binary Tree/Is A Binary Search Tree (2nd Approach).java
@@ -0,0 +1,154 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  public static class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data, Node left, Node right) {
+      this.data = data;
+      this.left = left;
+      this.right = right;
+    }
+  }
+
+  public static class Pair {
+    Node node;
+    int state;
+
+    Pair(Node node, int state) {
+      this.node = node;
+      this.state = state;
+    }
+  }
+
+  public static Node construct(Integer[] arr) {
+    Node root = new Node(arr[0], null, null);
+    Pair rtp = new Pair(root, 1);
+
+    Stack<Pair> st = new Stack<>();
+    st.push(rtp);
+
+    int idx = 0;
+    while (st.size() > 0) {
+      Pair top = st.peek();
+      if (top.state == 1) {
+        idx++;
+        if (arr[idx] != null) {
+          top.node.left = new Node(arr[idx], null, null);
+          Pair lp = new Pair(top.node.left, 1);
+          st.push(lp);
+        } else {
+          top.node.left = null;
+        }
+
+        top.state++;
+      } else if (top.state == 2) {
+        idx++;
+        if (arr[idx] != null) {
+          top.node.right = new Node(arr[idx], null, null);
+          Pair rp = new Pair(top.node.right, 1);
+          st.push(rp);
+        } else {
+          top.node.right = null;
+        }
+
+        top.state++;
+      } else {
+        st.pop();
+      }
+    }
+
+    return root;
+  }
+
+  public static void display(Node node) {
+    if (node == null) {
+      return;
+    }
+
+    String str = "";
+    str += node.left == null ? "." : node.left.data + "";
+    str += " <- " + node.data + " -> ";
+    str += node.right == null ? "." : node.right.data + "";
+    System.out.println(str);
+
+    display(node.left);
+    display(node.right);
+  }
+
+  public static int height(Node node) {
+    if (node == null) {
+      return -1;
+    }
+
+    int lh = height(node.left);
+    int rh = height(node.right);
+
+    int th = Math.max(lh, rh) + 1;
+    return th;
+  }
+
+  public static int max(Node node){
+    if(node == null){
+      return Integer.MIN_VALUE;
+    }
+
+    int lmax = max(node.left);
+    int rmax = max(node.right);
+
+    return Math.max(rmax, Math.max(lmax, node.data));
+  }
+
+  public static int min(Node node){
+    if(node == null){
+      return Integer.MAX_VALUE;
+    }
+
+    int lmin = min(node.left);
+    int rmin = min(node.right);
+
+    return Math.min(rmin, Math.min(lmin, node.data));
+  }
+
+  public static boolean isBST(Node node){
+   if(node == null){
+     return true;
+   }
+
+   int maxOfLeftSubTree = max(node.left);//getting max from left sub tree of current node
+   int minOfRightSubTree = min(node.right);//getting min from right sub tree of current node
+
+   boolean ltans = isBST(node.left);//getting ans from left sub tree of current node whether it is BST or not.
+   boolean rtans = isBST(node.right);//getting ans from right sub tree of current node whether it is BST or not.
+
+   //if currnt node + its left subtree + its right subtree are BST then return true otherwise false
+   if(node.data > maxOfLeftSubTree && node.data < minOfRightSubTree && ltans && rtans){
+     return true;
+   }
+   return false;
+  }
+  
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    Integer[] arr = new Integer[n];
+    String[] values = br.readLine().split(" ");
+    for (int i = 0; i < n; i++) {
+      if (values[i].equals("n") == false) {
+        arr[i] = Integer.parseInt(values[i]);
+      } else {
+        arr[i] = null;
+      }
+    }
+
+    Node root = construct(arr);
+    
+    // write your code here
+    boolean ans = isBST(root);
+    System.out.println(ans);
+  }
+
+}
