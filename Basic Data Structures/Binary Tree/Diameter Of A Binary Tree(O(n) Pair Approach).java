33ec3ad92025bde1734ed792708359dd186a637f
theGDM <gyandeepmehra370@gmail.com>
2022-03-30 19:46:55 +0530

    Create Diameter Of A Binary Tree(O(n) Pair Approach).java

---
+++ Basic Data Structures/Binary Tree/Diameter Of A Binary Tree(O(n) Pair Approach).java
@@ -0,0 +1,139 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  public static class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data, Node left, Node right) {
+      this.data = data;
+      this.left = left;
+      this.right = right;
+    }
+  }
+
+  public static class Pair {
+    Node node;
+    int state;
+
+    Pair(Node node, int state) {
+      this.node = node;
+      this.state = state;
+    }
+  }
+
+  public static Node construct(Integer[] arr) {
+    Node root = new Node(arr[0], null, null);
+    Pair rtp = new Pair(root, 1);
+
+    Stack<Pair> st = new Stack<>();
+    st.push(rtp);
+
+    int idx = 0;
+    while (st.size() > 0) {
+      Pair top = st.peek();
+      if (top.state == 1) {
+        idx++;
+        if (arr[idx] != null) {
+          top.node.left = new Node(arr[idx], null, null);
+          Pair lp = new Pair(top.node.left, 1);
+          st.push(lp);
+        } else {
+          top.node.left = null;
+        }
+
+        top.state++;
+      } else if (top.state == 2) {
+        idx++;
+        if (arr[idx] != null) {
+          top.node.right = new Node(arr[idx], null, null);
+          Pair rp = new Pair(top.node.right, 1);
+          st.push(rp);
+        } else {
+          top.node.right = null;
+        }
+
+        top.state++;
+      } else {
+        st.pop();
+      }
+    }
+
+    return root;
+  }
+
+  public static void display(Node node) {
+    if (node == null) {
+      return;
+    }
+
+    String str = "";
+    str += node.left == null ? "." : node.left.data + "";
+    str += " <- " + node.data + " -> ";
+    str += node.right == null ? "." : node.right.data + "";
+    System.out.println(str);
+
+    display(node.left);
+    display(node.right);
+  }
+
+  public static int height(Node node) {
+    if (node == null) {
+      return -1;
+    }
+
+    int lh = height(node.left);
+    int rh = height(node.right);
+
+    int th = Math.max(lh, rh) + 1;
+    return th;
+  }
+
+  public static class DIAPair{
+    int height;
+    int diameter;
+  }
+
+  public static DIAPair diameter1(Node node) {
+    if(node == null){ //base case
+      DIAPair bp = new DIAPair();
+      bp.height = -1;
+      bp.diameter = 0;
+      return bp;
+    }
+
+    DIAPair lp = diameter1(node.left); //getting height and diameter of left child of current node
+    DIAPair rp = diameter1(node.right); //getting height and diameter of right child of current node
+
+    //making current node answer
+    DIAPair curr = new DIAPair();
+    curr.height = Math.max(lp.height, rp.height) + 1;
+    
+    int dia = lp.height + rp.height + 2; //curr node height
+    curr.diameter = Math.max(dia, Math.max(lp.diameter, rp.diameter)); //updating max diameter in curr pair
+
+    return curr;
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    Integer[] arr = new Integer[n];
+    String[] values = br.readLine().split(" ");
+    for (int i = 0; i < n; i++) {
+      if (values[i].equals("n") == false) {
+        arr[i] = Integer.parseInt(values[i]);
+      } else {
+        arr[i] = null;
+      }
+    }
+
+    Node root = construct(arr);
+
+    DIAPair res = diameter1(root);
+    System.out.println(res.diameter);
+  }
+
+}
