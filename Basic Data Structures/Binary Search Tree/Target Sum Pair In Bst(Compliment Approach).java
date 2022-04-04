fd7f9df52f58a9b43c2116a8206db8f6c7dcd51a
theGDM <gyandeepmehra370@gmail.com>
2022-04-04 20:22:12 +0530

    Create Target Sum Pair In Bst(Compliment Approach).java

---
+++ Basic Data Structures/Binary Search Tree/Target Sum Pair In Bst(Compliment Approach).java
@@ -0,0 +1,138 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  public static class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data, Node left, Node right) {
+      this.data = data;
+      this.left = left;
+      this.right = right;
+    }
+  }
+
+  public static class Pair {
+    Node node;
+    int state;
+
+    Pair(Node node, int state) {
+      this.node = node;
+      this.state = state;
+    }
+  }
+
+  public static Node construct(Integer[] arr) {
+    Node root = new Node(arr[0], null, null);
+    Pair rtp = new Pair(root, 1);
+
+    Stack<Pair> st = new Stack<>();
+    st.push(rtp);
+
+    int idx = 0;
+    while (st.size() > 0) {
+      Pair top = st.peek();
+      if (top.state == 1) {
+        idx++;
+        if (arr[idx] != null) {
+          top.node.left = new Node(arr[idx], null, null);
+          Pair lp = new Pair(top.node.left, 1);
+          st.push(lp);
+        } else {
+          top.node.left = null;
+        }
+
+        top.state++;
+      } else if (top.state == 2) {
+        idx++;
+        if (arr[idx] != null) {
+          top.node.right = new Node(arr[idx], null, null);
+          Pair rp = new Pair(top.node.right, 1);
+          st.push(rp);
+        } else {
+          top.node.right = null;
+        }
+
+        top.state++;
+      } else {
+        st.pop();
+      }
+    }
+
+    return root;
+  }
+
+  public static void display(Node node) {
+    if (node == null) {
+      return;
+    }
+
+    String str = "";
+    str += node.left == null ? "." : node.left.data + "";
+    str += " <- " + node.data + " -> ";
+    str += node.right == null ? "." : node.right.data + "";
+    System.out.println(str);
+
+    display(node.left);
+    display(node.right);
+  }
+
+  public static void getInorder(Node root, ArrayList<Integer> in){
+    if(root == null){
+      return;
+    }
+    
+    getInorder(root.left, in);//pre me call
+    in.add(root.data);//in order work
+    getInorder(root.right, in);//post me call
+  }
+
+  public static boolean find(Node root, int val){
+    if(root == null) return false;
+
+    if(root.data > val) return find(root.left, val);
+    else if(root.data < val) return find(root.right, val);
+    else return true;
+  }
+
+  public static void targetSumPair(Node node, Node root, int target){
+    if(node == null){
+      return;
+    }
+
+    //since we need to print pairs in increasing order
+    //so we will the main task in inorder
+    targetSumPair(node.left, root, target);
+
+    int comp = target - node.data;//compliment
+    if(comp > node.data){
+      if(find(root, comp) == true){
+        System.out.println(node.data + " " + comp);
+      }
+    }
+    
+    targetSumPair(node.right, root, target);
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    Integer[] arr = new Integer[n];
+    String[] values = br.readLine().split(" ");
+    for (int i = 0; i < n; i++) {
+      if (values[i].equals("n") == false) {
+        arr[i] = Integer.parseInt(values[i]);
+      } else {
+        arr[i] = null;
+      }
+    }
+
+    int target = Integer.parseInt(br.readLine());
+
+    Node root = construct(arr);
+    targetSumPair(root, root, target);
+  }
+
+}
