8869e9079bda9273775a3fe31eb9045a3f11cddb
theGDM <gyandeepmehra370@gmail.com>
2022-05-15 13:36:03 +0530

    Update power_logarothmic.java

--- Recursion & Backtracking/Introduction to Recursion/power_logarothmic.java
+++ Recursion & Backtracking/Introduction to Recursion/power_logarothmic.java
@@ -1,7 +1,6 @@
 import java.util.*;
 
 public class power_logarothmic {
-
     public static void main(String[] args) throws Exception {
         // write your code here
         Scanner scn = new Scanner(System.in);
