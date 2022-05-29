c34c3b66eb9f2c5bbef85d6a9333181692d68bdf
theGDM <gyandeepmehra370@gmail.com>
2022-05-29 18:37:24 +0530

    Update Get Connected Components Of A Graph.java

--- Advanced Data Structure/Graphs/Get Connected Components Of A Graph.java
+++ Advanced Data Structure/Graphs/Get Connected Components Of A Graph.java
@@ -14,16 +14,6 @@ public class Main {
       }
    }
 
-   public static void getComponent(int src, ArrayList<Integer> comp, ArrayList<Edge>[] graph, boolean[] visited){
-      visited[src] = true;
-      comp.add(src);
-      for(Edge e : graph[src]){
-         if(visited[e.nbr] != true){
-            getComponent(e.nbr, comp, graph, visited);
-         }
-      }
-   }
-
    public static void main(String[] args) throws Exception {
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -46,13 +36,24 @@ public class Main {
       ArrayList<ArrayList<Integer>> comps = new ArrayList<>();
       boolean[] visited = new boolean[vtces];
       
-      for(int i = 0; i < vtces; ++i){
-         if(visited[i] != true){
+      for(int v = 0;v < vtces; ++v){
+         if(visited[v] != true){
             ArrayList<Integer> comp = new ArrayList<>();
-            getComponent(i, comp, graph, visited);
+            getConnectedComp(graph, v, visited, comp);
             comps.add(comp);
          }
       }
       System.out.println(comps);
+   }
+
+   public static void getConnectedComp(ArrayList<Edge>[] graph, int src, boolean[] visited, ArrayList<Integer> comp){
+      visited[src] = true;
+      comp.add(src);
+
+      for(Edge e : graph[src]){
+         if(visited[e.nbr] != true){
+            getConnectedComp(graph, e.nbr, visited, comp);
+         }
+      }
    }
 }
