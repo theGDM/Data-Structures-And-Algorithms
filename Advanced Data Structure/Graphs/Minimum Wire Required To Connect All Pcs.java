5bc13b4de25460efb06394d1e84fc4d2c994320f
theGDM <gyandeepmehra370@gmail.com>
2022-05-31 20:35:27 +0530

    Create Minimum Wire Required To Connect All Pcs.java

---
+++ Advanced Data Structure/Graphs/Minimum Wire Required To Connect All Pcs.java
@@ -0,0 +1,77 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+   static class Edge {
+      int src;
+      int nbr;
+      int wt;
+
+      Edge(int src, int nbr, int wt) {
+         this.src = src;
+         this.nbr = nbr;
+         this.wt = wt;
+      }
+   }
+
+   static class Pair implements Comparable<Pair>{
+      int v;
+      int av;
+      int wt;
+
+      Pair(int v, int av, int wt){
+         this.v = v;
+         this.av = av;
+         this.wt = wt;
+      }
+
+      public int compareTo(Pair o){
+         return this.wt - o.wt;
+      }
+   }
+
+   public static void main(String[] args) throws Exception {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+      int vtces = Integer.parseInt(br.readLine());
+      ArrayList<Edge>[] graph = new ArrayList[vtces];
+      for (int i = 0; i < vtces; i++) {
+         graph[i] = new ArrayList<>();
+      }
+
+      int edges = Integer.parseInt(br.readLine());
+      for (int i = 0; i < edges; i++) {
+         String[] parts = br.readLine().split(" ");
+         int v1 = Integer.parseInt(parts[0]);
+         int v2 = Integer.parseInt(parts[1]);
+         int wt = Integer.parseInt(parts[2]);
+         graph[v1].add(new Edge(v1, v2, wt));
+         graph[v2].add(new Edge(v2, v1, wt));
+      }
+
+      boolean[] visited = new boolean[vtces];
+      PriorityQueue<Pair> pq = new PriorityQueue<>();
+      pq.add(new Pair(0, -1, 0));
+
+      while(pq.size() != 0){
+         Pair rPair = pq.remove(); //remove
+         if(visited[rPair.v] == true){ //this vertex is aquired already from other path using min cost 
+            continue;
+         }
+
+         visited[rPair.v] = true; //mark visited
+
+         if(rPair.av != -1){ //work
+            System.out.println("[" + rPair.v + "-" + rPair.av + "@" + rPair.wt + "]");
+         }
+
+         for(Edge e : graph[rPair.v]){ //add unvisited nbrs
+            if(visited[e.nbr] == false){
+               pq.add(new Pair(e.nbr, rPair.v, e.wt));
+            }
+         }
+      }
+
+   }
+
+}
