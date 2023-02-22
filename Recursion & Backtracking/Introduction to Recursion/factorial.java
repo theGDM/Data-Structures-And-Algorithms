21ad72eeab68e7d701a15515ed1b8057c3be25d2
Gyandeep Mehra <gyandeepmehra370@gmail.com>
2023-02-22 23:56:29 +0530

    Update factorial.java

--- Recursion & Backtracking/Introduction to Recursion/factorial.java
+++ Recursion & Backtracking/Introduction to Recursion/factorial.java
@@ -1,7 +1,6 @@
 import java.util.*;
 
 public class factorial {
-
     public static void main(String[] args) throws Exception {
         // write your code here
         Scanner scn = new Scanner(System.in);
